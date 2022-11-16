38ff965a781e36762bd31c0b563d2d3f792baad5
Sultan Alotaibi <sul.otaibi@outlook.sa>
2022-11-16 12:44:58 +0300

    general enhancements

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Hero from './components/Hero';
 import About from './components/About';
 // import Projects from './components/Projects';
 import Contact from './components/Contact';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-QMX6EHT7TF');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
